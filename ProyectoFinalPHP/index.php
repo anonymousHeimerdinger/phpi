1fffe7b0985d2d9d002daf7d10f8ef33d41029e3
genis <anonymousheimer@gmail.com>
2018-12-29 23:59:39 +0100

    first commit

---
+++ ProyectoFinalPHP/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <!-- jQuery -->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- jQuery Validation-->
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
+    </head>
+    <body>
+        <form action="test.php" method="post">
+            <label>Nombre: </label>
+            <input type="text" name="name" id="name">
+            <label>DNI: </label>
+            <input type="text" name="dni" id="dni">
+            <script type="text/javascript">
+                //var dni = document.getElementById("dni").innerHTML;
+            </script>
+            <input type="submit" value="Crear sesión">
+        </form>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $.validator.addMethod('dni', function(value, element){
+        	var re = /^[A-z]?\d{8}[A-z]$/;
+        	return re.test(value);
+            });
+            
+            $('form').submit(function(e){
+        	//e.preventDefault();
+        	//ourWeb, journals, socialNetwork, webOffers, others
+        	
+            }).validate({
+        	debug: false,
+        	rules: {
+        		"name": {required: true},
+        		"dni": {required: true,
+        			dni: true}
+        	},
+        	messages: {
+        		"name": {required: "el campo esta vacío"},
+        		"dni": {required: "el campo esta vacío",
+        		dni: "dni incorrecto"}
+        	}
+            });
+        });
+        
+        //pattern="^((?=.*\d)|(?=.*[-.,;}{¿?*+_/=\)\(\)]))(?=.*[a-z])(?=.*[A-Z]).{8,}$"
+    </script>
+</html>
---
+++ ProyectoFinalPHP/test.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<?php
+class Person{
+    public $name;
+    private $dni;
+    
+    function _construct($_name, $_dni){
+        $this->name = $_name;
+        $this->dni = $_dni;
+    }
+    
+    function getName(){
+        return Person::$name;
+    }
+    
+    function getDni(){
+        return $this->dni;
+    }
+}
+
+class Candidates extends Person{
+    public $qualification;
+    
+    function getQualification(){
+        return $this->qualification;
+    }
+    
+    function setQualification($qualification){
+        $this->qualification = $qualification;
+    }
+}
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        session_start();
+        //session_destroy();
+        $sql = new mysqli('localhost', 'root', '', 'exam');
+        $sql->set_charset("utf8");
+        if($_POST && isset($_POST["name"], $_POST["dni"])){
+            /*$query = "SELECT * FROM candidates";
+            $tabla = $sql->query($query) or die ("Error de aplicacion: sentencia SQL no valida");
+            
+            while ($row = mysqli_fetch_row($tabla)){
+                if ($row[DNI]==$_POST["dni"]){
+                    echo "<option value='".$row[0]."'>".$row[0]."</option>";
+                }
+            }*/
+            $candidate = new Candidates($_POST["name"], $_POST["dni"]);
+            $_SESSION['name']=$_POST['name'];
+            $_SESSION['dni']=$_POST['dni'];
+            $_SESSION['pregunta1']='x';
+            $_SESSION['pregunta2']='x';
+            $_SESSION['pregunta3']='x';
+            $_SESSION['pregunta4']='x';
+            $_SESSION['pregunta5']='x';
+            $_SESSION['pregunta6']='x';
+            $_SESSION['pregunta7']='x';
+            $_SESSION['pregunta8']='x';
+            $_SESSION['pregunta9']='x';
+            $_SESSION['pregunta10']='x';
+        } else {
+            if ($_SESSION && isset($_SESSION["name"], $_SESSION["dni"])){
+                $candidate = new Candidates($_SESSION["name"], $_SESSION["dni"]);
+                //Pending cambiar preguntas y respuestas en función de lo que ya estaba contestado
+            } else {
+                //Falta hacer error
+                die("No se ha recibido nombre y DNI.");
+            }
+        }
+        ?>
+        
+        
+        
+        <form action="resultados.php" method="post">
+            <label></label>
+            <input type="radio" name="pregunta1a" value="">
+            <input type="radio" name="pregunta1b">
+            <input type="radio" name="pregunta1c">
+            <input type="radio" name="pregunta1d">
+            <label></label>
+            <input type="radio" name="pregunta2a">
+            <input type="radio" name="pregunta2b">
+            <input type="radio" name="pregunta2c">
+            <input type="radio" name="pregunta2d">
+            <label></label>
+            <input type="radio" name="pregunta3a">
+            <input type="radio" name="pregunta3b">
+            <input type="radio" name="pregunta3c">
+            <input type="radio" name="pregunta3d">
+            <label></label>
+            <input type="radio" name="pregunta4a">
+            <input type="radio" name="pregunta4b">
+            <input type="radio" name="pregunta4c">
+            <input type="radio" name="pregunta4d">
+            <label></label>
+            <input type="radio" name="pregunta5a">
+            <input type="radio" name="pregunta5b">
+            <input type="radio" name="pregunta5c">
+            <input type="radio" name="pregunta5d">
+            <label></label>
+            <input type="radio" name="pregunta6a">
+            <input type="radio" name="pregunta6b">
+            <input type="radio" name="pregunta6c">
+            <input type="radio" name="pregunta6d">
+            <label></label>
+            <input type="radio" name="pregunta7a">
+            <input type="radio" name="pregunta7b">
+            <input type="radio" name="pregunta7c">
+            <input type="radio" name="pregunta7d">
+            <label></label>
+            <input type="radio" name="pregunta8a">
+            <input type="radio" name="pregunta8b">
+            <input type="radio" name="pregunta8c">
+            <input type="radio" name="pregunta8d">
+            <label></label>
+            <input type="radio" name="pregunta9a">
+            <input type="radio" name="pregunta9b">
+            <input type="radio" name="pregunta9c">
+            <input type="radio" name="pregunta9d">
+            <label></label>
+            <input type="radio" name="pregunta10a">
+            <input type="radio" name="pregunta10b">
+            <input type="radio" name="pregunta10c">
+            <input type="radio" name="pregunta10d">
+            <input type="submit" value="Enviar respuestas">
+        </form>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
